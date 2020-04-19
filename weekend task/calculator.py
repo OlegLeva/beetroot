b143747c898076914439a90fe63a56c5c768726b
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-19 12:24:02 +0300

    calculator

--- weekend task/calculator.py
+++ weekend task/calculator.py
@@ -8,14 +8,13 @@ name = name.lower().title()
 
 operator = None
 
-
 while operator != 'x':
 
     operator = input((f'{name}, select an operation from the following list:\n'
                       f'Available operations:\naddition (+)\nsubtraction (-)\n'
                       f'multiplication (*)\ndivision (/)\ndivision is complete (//)'
                       f'\nremainder of division of numbers (%)\nraise the number to the degree (**)'
-                      f'\nround off the number\nfind the square of the number'
+                      f'\nround off the number (r)\nfind the square of the number (sq)'
                       f'\nexit the program (x)\nplease select the operation you need: '))
 
     if operator == '+':
@@ -33,14 +32,37 @@ while operator != 'x':
         x = float(input('Enter the first number: '))
         y = float(input('Enter the second number: '))
         print(f'Multiplying two numbers equals: {x * y}')
-
-    # rounding the first number to 'n' decimal places (where 'n' is the second number)
+    # Division
+    elif operator == '/':
+        x = float(input('Enter the first number: '))
+        y = float(input('Enter the second number: '))
+        print(f'Divisioning two numbers equals: {x / y}')
+    # The integer division
+    elif operator == '//':
+        x = float(input('Enter the first number: '))
+        y = float(input('Enter the second number: '))
+        print(f'The integer division of two numbers is equal to: {x // y}')
+    # Remainder of division of two numbers
+    elif operator == '%':
+        x = float(input('Enter the first number: '))
+        y = float(input('Enter the second number: '))
+        print(f'Remainder of division of two numbers is equal to: {x % y}')
+    # Raise the number to a degree
+    elif operator == '**':
+        x = float(input('Enter the number: '))
+        y = float(input('Enter the degree number: '))
+        print(f'The number in degree is: {x ** y}')
+    # Rounding the first number to n decimal places (when entering n - the second number)
     elif operator == 'r':
         x = float(input('Enter the first number: '))
-        z = int(input('Enter the second number: '))
-        print(f"rounding the first number to 'n' decimal places (where 'n' is the second number): {round(x.z)}")
+        y = int(input('Enter the second number: '))
+        print(f"rounding the first number to 'n' decimal places (where 'n' is the second number): {round(x,y)}")
+    # Find the square of the number
+    elif operator == 'sq':
+        x = float(input('Enter the number: '))
+        print(f'The square root of the number is: {x **(1/2)}')
 
-
+    # Exit the program
     elif operator == 'x':
         print('Thank you, i hope you enjoyed working)')
         break
