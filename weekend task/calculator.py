5921ce7b1ca9745f04d613b4bd6947748f48e2d8
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-19 23:34:11 +0300

    calculator

--- weekend task/calculator.py
+++ weekend task/calculator.py
@@ -3,9 +3,7 @@
 
 name = input('Please enter your name: ')
 
-#name = name.replace(" ", "") Хотел сразу так реализовать (но вдруг двойное имя типа "Аве Мария"))
-
-
+# Brings a name to a normal format
 name = name.split()
 name = ' '.join(name)
 name = name.lower().title()
@@ -21,55 +19,75 @@ while operator != 'x':
                       f'\nround off the number (r)\nfind the square of the number (sq)'
                       f'\nconverting from decimal to binary (b)\nexit the program (x)'
                       f'\nplease select the operation you need: '))
+    # Remove extra spaces
+    operator = operator.replace(" ", "")
 
     if operator == '+':
     # Addition
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'Addition of numbers is equal to: {x + y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'Addition of numbers is equal to: {float(x) + float(y)}')
     # subtracting
     elif operator == '-':
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'subtracting two numbers is equal to: {x - y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'subtracting two numbers is equal to: {float(x) - float(y)}')
     # Multiplying
     elif operator == '*':
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'Multiplying two numbers equals: {x * y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'Multiplying two numbers equals: {float(x) * float(y)}')
     # Division
     elif operator == '/':
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'Divisioning two numbers equals: {x / y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'Divisioning two numbers equals: {float(x) / float(y)}')
     # The integer division
     elif operator == '//':
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'The integer division of two numbers is equal to: {x // y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'The integer division of two numbers is equal to: {float(x) // float(y)}')
     # Remainder of division of two numbers
     elif operator == '%':
-        x = float(input('Enter the first number: '))
-        y = float(input('Enter the second number: '))
-        print(f'Remainder of division of two numbers is equal to: {x % y}')
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f'Remainder of division of two numbers is equal to: {float(x) % float(y)}')
     # Raise the number to a degree
     elif operator == '**':
-        x = float(input('Enter the number: '))
-        y = float(input('Enter the degree number: '))
-        print(f'The number in degree is: {x ** y}')
+        x = input('Enter the number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the degree number: ')
+        y = y.replace(" ", "")
+        print(f'The number in degree is: {float(x) ** float(y)}')
     # Rounding the first number to n decimal places (when entering n - the second number)
     elif operator == 'r':
-        x = float(input('Enter the first number: '))
-        y = int(input('Enter the second number: '))
-        print(f"rounding the first number to 'n' decimal places (where 'n' is the second number): {round(x,y)}")
+        x = input('Enter the first number: ')
+        x = x.replace(" ", "")
+        y = input('Enter the second number: ')
+        y = y.replace(" ", "")
+        print(f"rounding the first number to 'n' decimal places (where 'n' is the second number): {round(float (x),int(y))}")
     # Find the square of the number
     elif operator == 'sq':
-        x = float(input('Enter the number: '))
-        print(f'The square root of the number is: {x **(1/2)}')
+        x = input('Enter the number: ')
+        x = x.replace(" ", "")
+        print(f'The square root of the number is: {float(x) **(1/2)}')
     # Converting from decimal to binary
     elif operator == 'b':
-        x = int(input('Enter the number: '))
-        print(f'Decimal converted to binary: {bin(x)}')
+        x = input('Enter the number: ')
+        x = x.replace(" ", "")
+        print(f'Decimal converted to binary: {bin(int(x))}')
 
     # Exit the program
     elif operator == 'x':
