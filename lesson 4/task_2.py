38cf6c1aac8b141674495aae1eb41f94a718a83f
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-25 09:02:42 +0300

    update

--- lesson 4/task_2.py
+++ lesson 4/task_2.py
@@ -1,19 +1,11 @@
-# Поки не вийшло
+
 
 import random
-l1 = [random.randint(0,10) for r in range(10)]
-l2 = [random.randint(0,10) for t in range(10)]
-res = []
-i, j, n, = 0, 0, 0
+#l1 = set([random.randint(0,10) for r in range(10)])
+#l2 = set([random.randint(0,10) for t in range(10)])
+l1 = set([1,5,6,3,4,5,6,3,4,1])
+l2 = set([9,8,9,7,4,4,5,6,3,3])
+l3 = l1.intersection(l2)
+print(list(l3))
 
-while i < len(l1) and j < len(l2):
-    if l1[i] == l2[j]:
-        res.append(l1[i])
-        i += 1
-    else:
-        pass
-        j += 1
-print(l1)
-print(l2)
-print(res)
 
