92f36bf87f6c78f1fdb78a84580259e4471859d5
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-28 14:25:51 +0300

    simplified the decision

--- lesson 4/task_3.py
+++ lesson 4/task_3.py
@@ -1,16 +1,12 @@
 
+#simplified the decision
 l1 = [i for i in range(1,101)]
 j = 0
-r = 0
 res = []
 while j < len(l1)-1:
-    if l1[j] % 7 == 0:
+    if l1[j] % 7 == 0 and l1[j] % 5:
         res.append(l1[j])
     j += 1
-while r < len(res)-1:
-    if res[r] % 5 == 0:
-        del res[r]
-    r += 1
 
 print(res)
 
