efa34a67ecf84f7ec05640a6afb27a30d9eb0762
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-28 13:38:22 +0300

    update

--- lesson 3/task_1.py
+++ lesson 3/task_1.py
@@ -2,11 +2,14 @@
 #String manipulation
 
 s1 = input("Please enter 'helloworld': ")
+s1 = s1.strip(' ')
 print(s1[:2] + s1[-2:])
 
 s2 = input("Please enter 'my': ")
+s2 = s2.strip(' ')
 print(s2*2)
 
 s3 = input("Please enter'x': ")
+s3 = s3.strip(' ')
 if len('x') < 2:
     print('')
