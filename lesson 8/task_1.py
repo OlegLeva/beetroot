972e66f649281b15947a6158bdbe16f512202b22
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-05-05 22:27:19 +0300

    lesson_8

---
+++ lesson 8/task_1.py
@@ -0,0 +1,54 @@
+
+def oops():
+    raise IndexError
+
+def catch(x):
+    return x
+
+try:
+    catch(oops())
+except IndexError:
+    print('IndexError caught')
+
+###############################
+
+
+def oops1():
+    t = (5, 4, 4, 7, 8)
+    return t[8]
+
+def catch1(z):
+    return z
+
+try:
+    catch1(oops1())
+except IndexError:
+    print('IndexError caught')
+
+#################################
+
+
+def oops2():
+    raise KeyError
+
+def catch2(y):
+    return y
+
+try:
+    catch2(oops2())
+except IndexError:
+    print('IndexError caught')
+
+#################################
+
+def oops3():
+    q = {'J': 2, 'p': 3}
+    return q['m']
+
+def catch3(k):
+    return k
+
+try:
+    catch3(oops3())
+except IndexError:
+    print('IndexError caught')
---
+++ lesson 8/task_2.py
@@ -0,0 +1,14 @@
+def opr():
+    """ catch ValueError and ZeroDivisionError """
+    try:
+        a = input("Enter the number 'a': ")
+        b = input("Enter the number 'b': ")
+        z = (float(a) ** 2) / float(b)
+        return z
+    except ValueError:
+        return 'You entered not a number!'
+    except ZeroDivisionError:
+        return 'Only stupid divide by zero'
+
+
+print(opr())
