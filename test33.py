9fd149204bfdc12cf106bc55f7d0d38749995f1d
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-05-08 21:59:26 +0300

    cal

--- test33.py
+++ test33.py
@@ -1,12 +1,19 @@
+print('NEW')
+new_file = open('new_file2', 'w+')
+new_file.write('abcdefg')
+print(f'Cursor: Symbol #{new_file.tell()}')
+new_file.write('\nabcdefg')
+print(f'Cursor: Symbol #{new_file.tell()}')
+new_file.seek(1)
+new_file.write('456')
+print(f'Cursor: Symbol after write #{new_file.tell()}')
+print(new_file.read(3))
+print(f'Cursor: Symbol after read #{new_file.tell()}')
+new_file.write('456')
+print(f'Cursor: Symbol after write #{new_file.tell()}')
+new_file.close()
 
-def opr():
-    """ catch ValueError and ZeroDivisionError """
-    try:
-        a = input("Enter the number 'a': ")
-        b = input("Enter the number 'b': ")
-        z = (float(a) ** 2) / float(b)
-        return z
 
-    except:
-        return 'Only stupid divide by zero'
-print(opr())
+
+
+
