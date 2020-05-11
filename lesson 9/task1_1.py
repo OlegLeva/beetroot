ebbc8fe83cba54be409f9c268a858f4be19b4c6d
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-05-11 18:05:20 +0300

    lesson9 task1

---
+++ lesson 9/task1_1.py
@@ -0,0 +1,4 @@
+
+f = open('myfile.txt', 'r')
+print(f.read())
+f.close()
---
+++ lesson 9/task_1.py
@@ -0,0 +1,11 @@
+
+f = open('myfile.txt', 'w')
+f.write("Hello file world!\n")
+
+f.close()
+
+f = open('E:\Python\\beetroot\lesson 8\myfile.txt', 'w')
+f.write("Hello file world!\n")
+
+f.close() #Создался такой же файл в указаной директории.
+
