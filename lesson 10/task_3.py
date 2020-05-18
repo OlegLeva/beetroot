f6ffd6e4dc00bf418bcd796bd75d86515414f7cf
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-05-19 00:35:08 +0300

    task3 update

--- lesson 10/task_3.py
+++ lesson 10/task_3.py
@@ -1,66 +1,66 @@
-
-CHANNELS = ["BBC", "Discovery", "TV1000VIP", "TV1000"]
-
-
 class TVController:
-    pass
-
-
-    def __init__(self, BBC, Discovery, TV1000VIP, TV1000):
-        self.BBC = BBC
-        self.Discovery = Discovery
-        self.TV1000VIP = TV1000VIP
-        self.TV1000 = TV1000
-
+    CHANNELS = ["BBC", "Discovery", "TV1000VIP", "TV1000"]
+    number = 0
+    i = ""
 
     def first_chanel(self):
-        return CHANNELS[i]
+        self.number = 1
+        self.i = self.CHANNELS[self.number - 1]
+        return self.i
 
     def last_channel(self):
-        i = -1
-        return CHANNELS[i]
+        self.number = len(self.CHANNELS)
+        self.i = self.CHANNELS[self.number - 1]
+        return self.i
 
     def turn_channel(self):
-        i = int(n) - 1
-        return CHANNELS[i]
+        self.number = int(n) - 1
+        self.i = self.CHANNELS[self.number]
+        return self.i
 
     def next_channel(self):
-        global i
-        if CHANNELS[i] == CHANNELS[-1]:
-            i = 0
-            return CHANNELS[i]
+        if self.number == len(self.CHANNELS):
+            self.number = 1
+            self.i = self.CHANNELS[self.number - 1]
+            return self.i
         else:
-            return CHANNELS[i+1]
+            self.number += 1
+            self.i = self.CHANNELS[self.number - 1]
+            return self.i
 
     def previous_channel(self):
-        global i
-        if CHANNELS[i] == CHANNELS[0]:
-            i = -1
-            return CHANNELS[i]
+        if self.number == 1:
+            self.number = len(self.CHANNELS)
+            self.i = self.CHANNELS[self.number - 1]
+            return self.i
         else:
-            return CHANNELS[i-1]
+            self.number -= 1
+            self.i = self.CHANNELS[self.number - 1]
+            return self.i
 
     def current_channel(self):
-        return CHANNELS[i]
+        self.i = self.CHANNELS[self.number]
+        return self.i
 
-i = 0
-controller = TVController(*CHANNELS)
+
+controller = TVController()
 
 while True:
 
     n = input("Enter command or channel number: ")
 
-    if n == "first":
+    if n == "f":
         print(controller.first_chanel())
-    if n == "last":
+    if n == "l":
         print(controller.last_channel())
     if n in "1234":
         print(controller.turn_channel())
-    if n == "next":
+    if n == "nx":
         print(controller.next_channel())
-    if n == "back":
+    if n == "b":
         print(controller.previous_channel())
-
+    if n == "c":
+        print(controller.current_channel())
 
     if n == "x":
         break
