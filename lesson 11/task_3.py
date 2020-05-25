3045a3552c209a5c80135a55fa51e5c603c35a20
OlegLeva <oleg.levitskiy@gmail.com>
2020-05-25 16:40:10 +0300

    task 3

--- lesson 11/task_3.py
+++ lesson 11/task_3.py
@@ -5,7 +5,6 @@ class Product:
     amount = 0
     name_price = {}
 
-
     def __init__(self, type_prod, name_prod, price):
         self.type_prod = type_prod
         self.name_prod = name_prod
@@ -13,8 +12,16 @@ class Product:
         self.name_price = {}
 
 
+
 class ProductStore(Product):
     pass
 
-    def add(self, name, price, name_prod=None):
-        self.name_price[name_prod] = price*1.3
+    def add(self, price, name_prod):
+        self.name_price[name_prod] = price * 1.3
+
+
+
+avocado = ProductStore("fruit", 25.0, "Avocado")
+avocado.add(25, "Avocado")
+
+
