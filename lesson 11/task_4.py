0d17a170f003b0b6590ce82d2c29e56c8b38e611
OlegLeva <oleg.levitskiy@gmail.com>
2020-05-29 14:03:21 +0300

    task 4

--- lesson 11/task_4.py
+++ lesson 11/task_4.py
@@ -6,11 +6,11 @@ class CustomException(Exception):
         x.write(msg + "\n")
         x.close()
 
-    # def __str__(self):
-    #     return 'My exception'
-    #
-    # def __repr__(self):
-    #     return 'My exception'
+    def __str__(self):
+        return f'My exception: {"My CustomException"}'
+
+    def __repr__(self):
+        return f'My exception: {"My CustomException"}'
 try:
     raise CustomException("My CustomException")
 except CustomException as e:
