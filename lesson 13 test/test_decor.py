34e361ee53e75fd5c1d3355f8860e86159e72a82
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-02 12:16:55 +0300

    test_decor.py update

--- lesson 13 test/test_decor.py
+++ lesson 13 test/test_decor.py
@@ -1,39 +1,45 @@
-def sq(func):
-    l1 = func()
+import time
 
-    def wrapper():
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(time.time() - start)
+        return result
+    return wrapper
+
+def sq(func):
+    def wrapper(*args, **kwargs):
+        l1 = func(*args, **kwargs)
         l2 = [i ** 2 for i in l1]
         return l2
-
     return wrapper
 
 
 def div(func):
-    l1 = func()
-
-    def wrapper():
+    def wrapper(*args, **kwargs):
+        l1 = func(*args, **kwargs)
         l2 = [i - 1 for i in l1]
         return l2
-
     return wrapper
 
 
 def add_5b(func):
-    l1 = func()
-
-    def wrapper():
+    def wrapper(*args, **kwargs):
+        l1 = func(*args, **kwargs)
         l2 = [{"multiple_5": i} for i in l1 if i % 5 == 0]
         l3 = [{"multiple_3": i} for i in l1 if i % 3 == 0]
         return f"{l2}, \n{l3}"
-
     return wrapper
+
 
 
 @add_5b
 @div
 @sq
-def get_list():
-    l = [x for x in range(100) if x % 2 == 0]
+@time_it
+def get_list(list_arg):
+    l = [x for x in list_arg if x % 2 == 0]
     return l
 
-print(get_list())
+print(get_list(range(100**2)))
--- test33.py
+++ test33.py
@@ -1,18 +1,10 @@
-print('NEW')
-new_file = open('new_file2', 'w+')
-new_file.write('abcdefg')
-print(f'Cursor: Symbol #{new_file.tell()}')
-new_file.write('\nabcdefg')
-print(f'Cursor: Symbol #{new_file.tell()}')
-new_file.seek(1)
-new_file.write('456')
-print(f'Cursor: Symbol after write #{new_file.tell()}')
-print(new_file.read(3))
-print(f'Cursor: Symbol after read #{new_file.tell()}')
-new_file.write('456')
-print(f'Cursor: Symbol after write #{new_file.tell()}')
-new_file.close()
-
+def is_isogram(string):
+    x = set(string)
+    if len(x) == len(string):
+        return True
+    elif len(x) != len(string):
+        return False
+print(is_isogram("Rom"))
 
 
 
