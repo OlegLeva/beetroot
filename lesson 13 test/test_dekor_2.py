0bee5f38b66766fb4892d9b1e73e459316df9bc1
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-03 23:50:26 +0300

    test decorator 2

--- lesson 13 test/test_dekor_2.py
+++ lesson 13 test/test_dekor_2.py
@@ -1,28 +1,30 @@
-class Robot:
+"""class OurClass:
 
-    def __init__(self, name, build_year, lk=0.5, lp=0.5):
-        self.name = name
-        self.build_year = build_year
-        self.__potential_physical = lk
-        self.__potential_psychic = lp
+    def __init__(self, a):
+        self.OurAtt = a
+
+
+x = OurClass(10)
+print(x.OurAtt)"""
+
+class OurClass:
+
+    def __init__(self, a):
+        self.OurAtt = a
 
     @property
-    def condition(self):
-        s = self.__potential_physical + self.__potential_psychic
-        if s <= -1:
-            return "I feel miserable!"
-        elif s <= 0:
-            return "I feel bad!"
-        elif s <= 0.5:
-            return "Could be worse!"
-        elif s <= 1:
-            return "Seems to be okay!"
+    def OurAtt(self):
+        return self.__OurAtt
+
+    @OurAtt.setter
+    def OurAtt(self, val):
+        if val < 0:
+            self.__OurAtt = 0
+        elif val > 1000:
+            self.__OurAtt = 1000
         else:
-            return "Great!"
+            self.__OurAtt = val
 
 
-if __name__ == "__main__":
-    x = Robot("Marvin", 1979, 0.2, 0.4)
-    y = Robot("Caliban", 1993, -0.4, 0.3)
-    print(x.condition)
-    print(y.condition)
+x = OurClass(-1)
+print(x.OurAtt)
