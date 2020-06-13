110666ca59bd331fb6757216cde6ed369335d9c4
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-14 00:06:04 +0300

    homework17

--- lesson17/task3.py
+++ lesson17/task3.py
@@ -2,10 +2,12 @@ from lesson17_file.mymod import count_lines, count_chars, test
 import sys
 
 sys.path.insert(1, "/Users/mymac/PycharmProjects/beetroot/lesson17_file/mymod.py")
+# не хочет работать, или я не умею обращаться с терминалом(((
+#Думаю что PYTHONPATH включать каталог нужно, что бы можно было к нему обращаться по индексу.
 
 
 print(count_lines("text.txt"))
 print(count_chars("text.txt"))
 print(test("text.txt"))
 
-#Наблюдаются постоянные проблемы работы с терминалом(((
+#Наблюдаются постоянные проблемы неумения работы с терминалом(((
