c4f499c59a90558539feff76a1ac24bef9d1bdba
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-24 00:07:20 +0300

    homework 21

---
+++ lesson21/task1.py
@@ -0,0 +1,13 @@
+from typing import Optional, Union
+
+
+def to_power(x: Union[int, float], exp: int) -> Union[int, float]:
+    if exp == 0:
+        return 1
+    if exp < 0:
+        raise ValueError('This function works only with exp > 0.')
+    return x * to_power(x, exp - 1)
+
+
+print(to_power(2, 3))
+print(to_power(2, -5))
