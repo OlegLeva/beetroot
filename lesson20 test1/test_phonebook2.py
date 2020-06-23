fe5e586568e30c251a7006d873a809a220c2aab4
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-23 19:08:52 +0300

    update

--- lesson20 test1/test_phonebook2.py
+++ lesson20 test1/test_phonebook2.py
@@ -4,5 +4,5 @@ import unittest
 class testPhonebook2(unittest.TestCase):
 
     def test_first_name_add_f(self):
-        exspected_name = ???????
+        exspected_name = 'Oleg' #???????
         self.assertEqual(exspected_name, 'Oleg')
