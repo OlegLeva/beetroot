9254fe160e182d222eb0d5936067a26ebfe6d05b
OlegLeva <oleg.levitskiy@gmail.com>
2020-06-26 11:04:24 +0300

    calculatorQT.py

--- lesson22/mycalculatorQT.py
+++ lesson22/mycalculatorQT.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5.QtWidgets import (QApplication,
-                             QWidget,)
+                             QWidget,
+                             QLabel)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWidget = QWidget()
     mainWidget.setWindowTitle('Calculator1.0')
+    mainWidget.setGeometry(0, 0, 500, 300)
+    digitLabel = QLabel('12-DIGIT', parent=mainWidget)
     mainWidget.show()
+    return_code = app.exec()
+    sys.exit(return_code)
