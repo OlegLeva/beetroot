4ec743d4903c17ce79c89b564680c0e5495c3545
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-10 23:51:10 +0300

    update

--- lesson24/task2.py
+++ lesson24/task2.py
@@ -1,6 +1,6 @@
 # Захотелось реализовать без класса.
 
-
+'''
 def check_balanced(items):
     stack = []
     for item in items:
@@ -18,6 +18,37 @@ def check_balanced(items):
 
 print(check_balanced("((()))"))
 print(check_balanced("(()))"))
+print(check_balanced("((())"))'''
+
+
+def check_balanced(items):
+    stack = []
+    for item in items:
+        if item == "(" or item == "[" or item == "{":
+            stack.append(item)
+        elif item == ")" or item == "]" or item == "}":
+            if stack == []:
+                return False
+            elem = stack.pop()
+            if not comparison(elem, item):
+                return False
+
+    if stack != []:
+        return False
+    else:
+        return True
+
+
+def comparison(opening, closing):
+    if opening == "(" and closing == ")":
+        return True
+    if opening == "[" and closing == "]":
+        return True
+    if opening == "{" and closing == "}":
+        return True
+    return False
+
+
+print(check_balanced("([]([({})]){})"))
+print(check_balanced("(({(])}))"))
 print(check_balanced("((())"))
-
-
