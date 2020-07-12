6ef649a652a850493d10372585f69261f4fc4143
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-12 22:56:04 +0300

    homework25

--- lesson26/test_server.py
+++ lesson26/test_server.py
@@ -1,17 +1,8 @@
 import socket
-
 HOST = '127.0.0.1'
-PORT = 65432
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+PORT = 6789
+sock_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_server.bind((HOST, PORT))
+while True:
+    data, addr = sock_server.recvfrom(1024)
+    print(f"Message: {data.decode('utf-8')}") # Добавив "decode" бо виводило "b" перед "Hello"
