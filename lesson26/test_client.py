0344eaf13e617cd519a672490fd7cf6ff72a588c
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-12 18:52:29 +0300

    homework25

--- lesson26/test_client.py
+++ lesson26/test_client.py
@@ -1,8 +1,8 @@
 import socket
 
-HOST = '192.168.0.133'
+HOST = '127.0.0.1'
 PORT = 65432
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Privet Olya')
     data = s.recv(1024)
--- lesson26/test_server.py
+++ lesson26/test_server.py
@@ -1,10 +1,10 @@
 import socket
 
-HOST = '192.168.0.133'
+HOST = '127.0.0.1'
 PORT = 65432
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
