b035ea8e8fb6c01fd34a1e06350a417bbffc08b7
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-15 16:32:01 +0300

    homework27

--- lesson27/task3.py
+++ lesson27/task3.py
@@ -1,5 +1,20 @@
 import requests
 
-r = requests.get('https://openweathermap.org.')
+def weather_data(query):
+    res = requests.get(f'https://openweathermap.org/find?utf8=%E2%9C%93&q={query}')
+    return res.text
 
-r.text
+
+def print_weather(result, city):
+    print(f"{}")
+
+
+def main():
+    city = input("Введите пожалуйста город: ")
+    query = city
+    weath = weather_data(query)
+    print_weather(weath, city)
+
+
+if __name__ == '__main__':
+    main()
