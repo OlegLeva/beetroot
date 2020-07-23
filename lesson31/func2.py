57a403f377e9edb9669448228dab701aa392069e
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-23 16:25:20 +0300

    homework31

--- lesson31/func2.py
+++ lesson31/func2.py
@@ -2,15 +2,20 @@ import psycopg2
 
 connection = psycopg2.connect(host='localhost', database='postgres', port=5432,
                               user='postgres', password='levon4202099')
-id = int(input("Enter id: "))
-first_name = input("Enter first_name: ")
-last_name = input("Enter last_name: ")
-full_name = input("Enter full_name: ")
-phone = input("Enter phone: ")
-region = input("Enter region: ")
-city = input("Enter city: ")
+table_name = input("Enter table name for data entry: ")
+if table_name == "country":
+    def add_country():
+        id = int(input("Enter id: "))
+        country_name = input("The name of the country: ")
+        abbr = input("Enter the abbreviation of the country: ")
+        phone_code = input("Enter phone code: ")
+        cursor = connection.cursor()
+        cursor.execute(f'INSERT INTO country(id, country_name, abbr, phone_code) '
+                       f'VALUES((%s), (%s), (%s), (%s))',
+                       (id, country_name, abbr, phone_code))
 
-cursor = connection.cursor()
-cursor.execute(f'INSERT INTO public."Phonebook"(id, first_name, last_name, full_name, phone, region, city) '
-               f'VALUES((%s), (%s), (%s), (%s), (%s), (%s), (%s))',
-               (id, first_name, last_name, full_name, phone, region, city))
+        connection.commit()
+        cursor.close()
+
+
+    add_country()
