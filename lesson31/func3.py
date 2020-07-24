f7815a1b821cb7b105eb0b9a5314a07a102399cd
OlegLeva <oleg.levitskiy@gmail.com>
2020-07-24 15:53:23 +0300

    homework31

--- lesson31/func3.py
+++ lesson31/func3.py
@@ -6,15 +6,18 @@ connection = psycopg2.connect(host='localhost', database='postgres', port=5432,
 
 def create_country():
     cursor = connection.cursor()
-    cursor.execute('CREATE TABLE "country" (id int, country_name varchar(40), abbr varchar(3), phone_code varchar(4))')
+    cursor.execute('CREATE TABLE "country" (id int NOT NULL, country_name varchar(40) NOT NULL, '
+                   'abbr varchar(3) NOT NULL, phone_code varchar(4) NOT NULL, PRIMARY KEY(id))')
     connection.commit()
     cursor.close()
     connection.close()
 
 
+
 def create_city():
     cursor = connection.cursor()
-    cursor.execute('CREATE TABLE "city" (id int, city_name varchar(40), country_id int)')
+    cursor.execute('CREATE TABLE "city" (id int NOT NULL, city_name varchar(40) NOT NULL, '
+                   'country_id int NOT NULL, PRIMARY KEY(id))')
     connection.commit()
     cursor.close()
     connection.close()
@@ -22,10 +25,11 @@ def create_city():
 
 def create_person():
     cursor = connection.cursor()
-    cursor.execute('CREATE TABLE "person" (id int, name varchar(40), city_id int)')
+    cursor.execute('CREATE TABLE "person" (id int NOT NULL, name varchar(40) NOT NULL, '
+                   'city_id int NOT NULL, PRIMARY KEY(id))')
     connection.commit()
     cursor.close()
     connection.close()
 
 
-create_country()
+
