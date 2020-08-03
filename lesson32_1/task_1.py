b6ec16b3bff1b3d10626db7465a4972dd71e0be6
OlegLeva <oleg.levitskiy@gmail.com>
2020-08-03 23:35:08 +0300

    homework 32 вроде final

--- lesson32_1/task_1.py
+++ lesson32_1/task_1.py
@@ -2,50 +2,47 @@ import asyncio
 
 
 async def fib():
-    await asyncio.sleep(1)
     lst_fib = []
     a, b = 0, 1
     for i in range(10):
         a, b = b, a + b
+        await asyncio.sleep(0.1)
         lst_fib.append(a)
     print(lst_fib)
 
 
 async def factorial():
-    await asyncio.sleep(1)
     fac, i = 1, 0
     lst_fac = []
     for m in range(10):
         while i < m:
             i += 1
             fac = fac * i
+            await asyncio.sleep(0.1)
             lst_fac.append(fac)
     print(lst_fac)
 
 
-async def sqrt():
-    await asyncio.sleep(1)
-    lst_sqrt = []
-    for i in range(10):
-        s = i ** 2
-        lst_sqrt.append(s)
-    print(lst_sqrt)
-
-
-async def cubic():
-    await asyncio.sleep(1)
-    lst_cub = []
-    for i in range(10):
-        s = i ** 3
-        lst_cub.append(s)
-    print(lst_cub)
+async def expo(x):
+    lst_expo = []
+    for p in range(1, 11):
+        m = x
+        t = 1
+        while p:
+            if p % 2:
+                t *= m
+            m *= m
+            p //= 2
+        await asyncio.sleep(0.1)
+        lst_expo.append(t)
+    print(lst_expo)
 
 
 async def main():
     task1 = asyncio.create_task(fib())
     task2 = asyncio.create_task(factorial())
-    task3 = asyncio.create_task(sqrt())
-    task4 = asyncio.create_task(cubic())
+    task3 = asyncio.create_task(expo(2))
+    task4 = asyncio.create_task(expo(3))
 
     await asyncio.gather(task1, task2, task3, task4)
 
--- lesson32_1/task_2.py
+++ lesson32_1/task_2.py
@@ -5,60 +5,27 @@ import json
 com_lst = []
 
 
-async def second_post():
-    await asyncio.sleep(0.1)
-    resp = await requests.get('https://api.pushshift.io/reddit/comment/search/',
-                              params={'subreddit': 'interestingasfuck', 'size': 25, 'sort': 'new'})
-
+async def get_comment(topic):
+    resp = await requests.get('http://api.pushshift.io/reddit/comment/search/',
+                              params={'subreddit': topic, 'size': 25, 'sort': 'new'})
     res = await resp.json()
     comment_lst = []
     for comment in res['data']:
-        comment_lst.append(comment['body'].encode())
+        comment_lst.append(comment['body'])
     key_lst = list(range(1, 26))
     my_dict = dict(zip(key_lst, comment_lst))
-    return com_lst.append(my_dict)
-
-
-async def first_post():
-    await asyncio.sleep(0.1)
-    resp = await requests.get('https://api.pushshift.io/reddit/comment/search/',
-                              params={'subreddit': 'nextfuckinglevel', 'size': 25, 'sort': 'new'})
-
-    res = await resp.json()
-    comment_lst = []
-    for comment in res['data']:
-        comment_lst.append(comment['body'].encode())
-    key_lst = list(range(1, 26))
-    my_dict = dict(zip(key_lst, comment_lst))
-    return com_lst.append(my_dict)
-
-
-async def third_post():
-    await asyncio.sleep(0.1)
-    resp = await requests.get('https://api.pushshift.io/reddit/comment/search/',
-                              params={'subreddit': 'Minecraft', 'size': 25, 'sort': 'new'})
-
-    res = await resp.json()
-    comment_lst = []
-    for comment in res['data']:
-        comment_lst.append(comment['body'].encode())
-    key_lst = list(range(1, 26))
-    my_dict = dict(zip(key_lst, comment_lst))
-    return com_lst.append(my_dict)
-
-
-with open('comment_json.json', 'wb') as file:
-    json.dump(com_lst, file, indent=4)
+    com_lst.append(my_dict)
 
 
 async def main():
-    task1 = asyncio.create_task(second_post())
-    task2 = asyncio.create_task(first_post())
-    task3 = asyncio.create_task(third_post())
+    task1 = asyncio.create_task(get_comment('interestingasfuck'))
+    task2 = asyncio.create_task(get_comment('nextfuckinglevel'))
+    task3 = asyncio.create_task(get_comment('Minecraft'))
 
     await asyncio.gather(task1, task2, task3)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
-
+    with open('comment_json.json', 'w') as file:
+        json.dump(com_lst, file, indent=4)
