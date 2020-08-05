7cedab5b9889a7a68cf5eb3b33330bd037d8e505
OlegLeva <oleg.levitskiy@gmail.com>
2020-08-05 23:13:18 +0300

    homework 32 task_1.py update

--- lesson32_1/task_1.py
+++ lesson32_1/task_1.py
@@ -4,23 +4,23 @@ import asyncio
 async def fib():
     lst_fib = []
     a, b = 0, 1
-    for i in range(10):
+    for i in range(1, 11):
         a, b = b, a + b
         await asyncio.sleep(0.1)
         lst_fib.append(a)
-    print(lst_fib)
+    return lst_fib
 
 
 async def factorial():
     fac, i = 1, 0
     lst_fac = []
-    for m in range(10):
+    for m in range(1, 11):
         while i < m:
             i += 1
             fac = fac * i
             await asyncio.sleep(0.1)
             lst_fac.append(fac)
-    print(lst_fac)
+    return lst_fac
 
 
 async def expo(x):
@@ -35,7 +35,7 @@ async def expo(x):
             p //= 2
         await asyncio.sleep(0.1)
         lst_expo.append(t)
-    print(lst_expo)
+    return lst_expo
 
 
 async def main():
@@ -44,7 +44,8 @@ async def main():
     task3 = asyncio.create_task(expo(2))
     task4 = asyncio.create_task(expo(3))
 
-    await asyncio.gather(task1, task2, task3, task4)
+    res = await asyncio.gather(task1, task2, task3, task4)
+    print(res)
 
 
 if __name__ == '__main__':
