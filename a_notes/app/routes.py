12faf2b8340127ffb163212d3f807592c4565c6b
OlegLeva <oleg.levitskiy@gmail.com>
2020-09-07 21:10:33 +0300

    notes1

--- a_notes/app/routes.py
+++ a_notes/app/routes.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template,flash, redirect, url_for
 from a_notes.app.forms import LoginForm
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'password'
 
