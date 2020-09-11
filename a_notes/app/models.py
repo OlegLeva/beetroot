b4230a10dfe4586f879352ef23d2f5612497632b
Maksym Polshcha <maxp@sterch.net>
2020-09-11 13:44:27 -0400

    Update models.py
    models wireframe

--- a_notes/app/models.py
+++ a_notes/app/models.py
@@ -1,11 +1,28 @@
 from a_notes.app.routes import db
 
 
-class User(db.Model):
+class Notification(db.Model):
+    __tablename__ = 'notification'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    days_before = db.Column(db.Integer, nullable=False)
+    document = db.relationship("Document", uselist=False, backref="notification")
+        
+class Document(db.Model):
+    __tablename__ = 'document'
+    id = db.Column(db.Integer, primary_key=True)
+    exp_date = db.Column(db.Datetime, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    
+class Truck(db.Model):
+    __tablename__ = 'truck'
+    license_plate = db.Column(db.String, primary_key=True)
+    documents = db.relationship('Document', backref='truck', lazy=True)
+    
+class Driver(db.Model):
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+class Trailer(db.Model):
+
+    
+class AutoTrain(db.Model):
+    
+    
