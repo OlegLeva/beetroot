7f108b461ff03fc34b4fdd159e1b9680c8e1be22
OlegLeva <oleg.levitskiy@gmail.com>
2020-09-16 16:25:59 +0300

    windows

--- aa_notes/app/models.py
+++ aa_notes/app/models.py
@@ -12,7 +12,13 @@ class Document(db.Model):
     __tablename__ = 'document'
     id = db.Column(db.Integer, primary_key=True)
     exp_date = db.Column(db.Date, nullable=False)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    truck_id = db.Column(db.String, db.ForeignKey('truck.license_plate'),
+                            nullable=False)
+    trailer_id = db.Column(db.String, db.ForeignKey('trailer.license_plate'),
+                         nullable=False)
+    driver_id = db.Column(db.Integer, db.ForeignKey('driver.id'),
+                         nullable=False)
     notification = db.relationship('Notification', backref='document', lazy=True)
 
 
@@ -24,7 +30,8 @@ class Truck(db.Model):
 
 class Driver(db.Model):
     __tablename__ = 'driver'
-    phone = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     document = db.relationship('Document', backref='driver', lazy=True)
 
@@ -40,10 +47,9 @@ class AutoTrain(db.Model):
     truck_id = db.Column(db.String, primary_key=True)
     trailer_id = db.Column(db.String, primary_key=True)
     driver_id = db.Column(db.Integer, primary_key=True)
-    driver_name = db.Column(db.String, nullable=False)
     truck = db.relationship('Truck', uselist=False, back_populates='autotrain')
     trailer = db.relationship('Trailer', uselist=False, back_populates='autotrain')
     driver = db.relationship('Driver', uselist=False, back_populates='autotrain')
 
     def __repr__(self):
-        return '<{} {} {} {}>'.format(self.truck_id, self.trailer_id, self.driver_name, self.driver_id)
+        return '<{} {} {}>'.format(self.truck_id, self.trailer_id, self.driver_id)
--- aa_notes/fill_db.py
+++ aa_notes/fill_db.py
@@ -2,22 +2,50 @@ from app import db
 
 # Todo: Создать базу данных один раз, вытащить базу данныз и отобразить на лицевой странице
 
-from app.models import Trailer
-from app.models import Truck
+from app.models import Driver, Document, Trailer, Truck, Notification
+
+truck = [
+    Truck(license_plate="BI4078CE"),
+    Truck(license_plate="BI8576EI"),
+    Truck(license_plate="BI7185AO")
+]
+for p in truck:
+    db.session.add(p)
+db.session.commit()
+
 trailers = [
-    Trailer(license_plate="AA1237BB"),
-    Trailer(license_plate="AA1238BB"),
-    Trailer(license_plate="AA1239BB"),
+    Trailer(license_plate="BI2324XK"),
+    Trailer(license_plate="BI3844XM"),
+    Trailer(license_plate="BI4663XK")
 ]
 for t in trailers:
     db.session.add(t)
 db.session.commit()
 
-truck = [
-    Truck(license_plate="BI4078BB"),
-    Truck(license_plate="BI8576BB"),
-    Truck(license_plate="BI7185BB"),
+drivers = [
+    Driver(phone="+380980353438", name="Терещенко Дмитрий"),
+    Driver(phone="+380679265219", name="Возный Андрей"),
+    Driver(phone="+380974537879", name="Возный Виталий")
 ]
-for p in truck:
-    db.session.add(p)
+for d in drivers:
+    db.session.add(d)
 db.session.commit()
+
+documemts = [
+    Document(name="protokol", exp_date="25.12.2020"),
+    Document(name="protokol", exp_date="15.12.2020"),
+    Document(phone="protocol", exp_date="05.11.2020")
+]
+for d in documemts:
+    db.session.add(d)
+db.session.commit()
+
+notifications = [
+    Notification(days_before=30),
+    Notification(days_before=30),
+    Notification(days_before=30)
+]
+for d in documemts:
+    db.session.add(d)
+db.session.commit()
+
