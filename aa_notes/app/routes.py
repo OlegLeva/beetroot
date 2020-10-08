24cc5c6648254e60d5b3167f914b34a9b0f61412
OlegLeva <oleg.levitskiy@gmail.com>
2020-10-08 16:33:26 +0300

    ((

--- aa_notes/app/routes.py
+++ aa_notes/app/routes.py
@@ -14,25 +14,51 @@ def index():
     return render_template('index.html', autotrains=autotrains, get_phone=get_phone, form=form)
 
 
+@app.route('/index/<int:autotrain_id>')
+def edit1(autotrain_id):
+    autotrain1 = AutoTrain.query.get(autotrain_id)
+
+    return render_template('edit1.html', autotrain=autotrain1)
+
+
+@app.route('/add_autotrain', methods=['GET', 'POST'])
+def add_autotrain():
+    if request.method == 'POST':
+        truck = Truck(license_plate=request.form['truck_license_plate'])
+        trailer = Trailer(license_plate1=request.form['trailer_license_plate'])
+        driver = Driver(id=request.form['driver_name'], phone=request.form['phone'])
+        train = AutoTrain(truck_id=truck.license_plate,
+                          trailer_id=trailer.license_plate1,
+                          driver_id=driver.id)
+
+        db.session.add(truck)
+        db.session.add(trailer)
+        db.session.add(driver)
+        db.session.add(train)
+        db.session.commit()
+
+        return render_template("/add_autotrain.html")
+
+
 @app.route('/add_train', methods=['GET', 'POST'])
 def add_train():
     truck = Truck(license_plate=request.form['truck_license_plate'])
-    trailer = Trailer(license_plate1=request.form['trailer_license_plate'])
+    trailer = Trailer(license_plate=request.form['trailer_license_plate'])
     driver = Driver(id=request.form['driver_name'], phone=request.form['phone'])
     train = AutoTrain(autotrain_id=request.form['id_autotrain'],
                       truck_id=truck.license_plate,
-                      trailer_id=trailer.license_plate1,
+                      trailer_id=trailer.license_plate,
                       driver_id=driver.id)
-# try:
-    db.session.add(truck)
-    db.session.add(trailer)
-    db.session.add(driver)
-    db.session.add(train)
-    db.session.commit()
+    try:
+        db.session.add(truck)
+        db.session.add(trailer)
+        db.session.add(driver)
+        db.session.add(train)
+        db.session.commit()
 
-    return redirect("/index")
-# except:
-#     return render_template("/add_train")
+        return redirect("/index")
+    except:
+        return redirect("/index")
 
 
 @app.route('/add_doc_truck', methods=['GET', 'POST'])
@@ -41,14 +67,13 @@ def add_doc_truck():
         name = request.form['name']
         exp_date = request.form['exp_date']
         truck_id = request.form['truck_id']
-
         document = Document(name=name, exp_date=exp_date, truck_id=truck_id)
 
-    # try:
-        db.session.add(document)
-        db.session.commit()
         days_before = request.form['days_before']
-        notified = Notification(id=(Document.query.filter_by(name='name').first().id), days_before=days_before)
+        notified = Notification(id=Document.query.filter_by(name='name').first().id, days_before=days_before)
+
+        # try:
+        db.session.add(document)
         db.session.add(notified)
         db.session.commit()
 
@@ -95,5 +120,3 @@ def add_doc_driver():
             return 'Введены неверные данные'
     else:
         return render_template('add_doc_driver.html')
-
-
